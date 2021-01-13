c65bc252f22f618ddb8224a4dc33e1251c07eb2f
Nikolas Damian Vasconcelos <nkvdamian@gmail.com>
2021-01-12 23:08:14 -0300

    App main layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Informational from './components/Informational';
+import PlanSelection from './components/PlanSelection';
+
+import './styles/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex flex-column flex-base">
+      <Header />
+      <Informational />
+      <PlanSelection />
     </div>
   );
 }
